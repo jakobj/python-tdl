b00120a56d910cb97df916086dfd05f3c72b43dc
Jakob Jordan <j.jordan@fz-juelich.de>
2017-01-28 22:55:35 +0100

    Update main simulation

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ import numpy as np
 import universe
 
 
-n_runs = 10
-n_steps = 10000
-uni = universe.Universe('grid_world', shape=(4, 4, ))
+seed = 91231
+n_trials = 20
+n_steps = 1000
 
-uni.set_reward([3, 0], 1.)
-uni.set_reward([1, 0], -1.)
-uni.set_reward([1, 1], -1.)
-uni.set_reward([1, 2], -1.)
+steps_to_reward = 9
+max_reward = n_steps // steps_to_reward * 5.
 
-for i in range(10):
+uni = universe.Universe('grid_world', world='2d_world0')
+uni.show()
+
+reward = []
+for i in range(n_trials):
     np.random.seed(1234 + i)
 
-    uni.reset_agent()
+    uni.reset_agent_position()
+    uni.reset_agent_reward()
     for _ in range(n_steps):
         uni.step()
 
-    print('total reward after {n} steps: {reward}'.format(n=(i + 1) * n_steps, reward=uni._agent._total_reward))
-
-uni.reset_agent()
-for _ in range(100):
-    uni.show()
-    uni.step()
+    reward.append(uni._agent._total_reward)
+    print('total reward after {steps} steps: {reward}/{max_reward}'.format(steps=n_steps, reward=uni._agent._total_reward, max_reward=max_reward))
