0c4bdc2d27fb289979004c7e0a0b9d0bf0a764cf
Jakob Jordan <j.jordan@fz-juelich.de>
2017-01-29 22:09:02 +0100

    Add trial cumulative reward plot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 import universe
@@ -9,14 +10,15 @@ seed = 91231
 n_trials = 20
 n_steps = 1000
 
-steps_to_reward = 9
+steps_to_reward = 14
 max_reward = n_steps // steps_to_reward * 5.
 
-uni = universe.Universe('grid_world', world='2d_world0')
+uni = universe.Universe('grid_world', world='2d_world1')
 uni.show()
 
 reward = []
 for i in range(n_trials):
+    reward_trial = []
     np.random.seed(1234 + i)
 
     uni.reset_agent_position()
@@ -24,5 +26,11 @@ for i in range(n_trials):
     for _ in range(n_steps):
         uni.step()
 
-    reward.append(uni._agent._total_reward)
+        reward_trial.append(uni._agent._total_reward)
+    reward.append(reward_trial)
     print('total reward after {steps} steps: {reward}/{max_reward}'.format(steps=n_steps, reward=uni._agent._total_reward, max_reward=max_reward))
+
+
+for i in range(n_trials):
+    plt.plot(reward[i])
+plt.show()
